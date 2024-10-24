d46967413028880afd807502ba7c0cdbd9894aff
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-24 16:10:16 +0200

    random numbers

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("JS OK");
+// Generare 5 numeri casuali da 1 a 99
+
+let listRandomNumber = [];
+
+function randomNumber() {
+  for (let i = 0; i < 5; i++) {
+    listRandomNumber.push(Math.floor(Math.random() * 99) + 1);
+  }
+  return listRandomNumber;
+}
+
+listRandomNumber = randomNumber();
+console.log(listRandomNumber);
